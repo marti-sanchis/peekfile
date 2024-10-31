731f04b959aa05c1444f439f3743908f5f06d5da
marti-sanchis <marti.sanchis01@estudiant.upf.edu>
2024-10-31 19:05:50 +0100

    second commit

--- countlines.sh
+++ countlines.sh
@@ -1,4 +1,6 @@
-nlines=$(cat "$1" | wc -l)
-filename=$(basename "$1")
-if [[ $nlines -eq 0 ]]; then echo File named $filename does not contain any line; elif [[ $nlines -eq 1 ]]; then echo File named $filename contains only 1 line ; else echo File named $filename contains '>1' lines; fi
+for file in "$@"; do 
+nlines=$(cat "$file" | wc -l)
+filename=$(basename "$file")
+if [[ $nlines -eq 0 ]]; then echo File named $filename does not contain any line; elif [[ $nlines -eq 1 ]]; then echo File named $filename contains only 1 line ; else echo File named $filename contains '>1' lines; fi; done
+
 
