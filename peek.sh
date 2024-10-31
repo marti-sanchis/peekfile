6f9601e6496535c9a39c6420d9e1a87a92cc22f8
marti-sanchis <marti.sanchis01@estudiant.upf.edu>
2024-10-31 15:16:42 +0100

    third commit

--- peek.sh
+++ peek.sh
@@ -1,1 +1,12 @@
-head -n $2 $1; echo ...; tail -n $2 $1
+if [[ -z "$2" ]]; then num_lines=3; else num_lines=$2; fi
+#head -n $num_lines $1; echo ...; tail -n $num_lines $1
+
+total_lines=$(wc -l < "$1")
+
+if [[ $total_lines -le $((2 * num_lines)) ]]; then cat "$1";
+else
+  echo "Warning: Showing only the first $num_lines and last $num_lines lines of a larger file."
+  head -n "$num_lines" "$1"
+  echo "..."
+  tail -n "$num_lines" "$1"; fi
+
